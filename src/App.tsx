4d49bd2005235f7e7ef640b564d8f35c45d82b22
Federico Macías <fma.abogado@gmail.com>
2022-11-18 10:08:46 -0600

    Creando el contexto para notificaciones de error y el useNotification

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import { Container, Button } from "@mui/material";
 import { Navbar } from "common/Navbar";
 import { BrowserRouter } from "react-router-dom";
 import { AppRouter } from "router";
+import { NotificationProvider } from "context/notification.context";
 
 function App() {
   return (
-    <BrowserRouter>
-      <AppRouter />
-    </BrowserRouter>
+    <NotificationProvider>
+      <BrowserRouter>
+        <AppRouter />
+      </BrowserRouter>
+    </NotificationProvider>
   );
 }
 
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { Container, Button } from "@mui/material";
+import { useNotification } from "../../context/notification.context";
 
 export const HomePage: React.FC<{}> = () => {
+  const { getError } = useNotification();
+  const handleClick = () => {
+    getError("Tienes un error en el backend");
+  };
   return (
     <Container sx={{ mt: 9 }} maxWidth="xl">
-      <Button fullWidth variant="contained">
+      <Button onClick={handleClick} fullWidth variant="contained">
         Estamos en el home
       </Button>
     </Container>
