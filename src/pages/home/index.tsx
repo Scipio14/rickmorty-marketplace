014185d7fd3e79fae84ca1edcf4e2373a43d35fc
Federico Macías <fma.abogado@gmail.com>
2022-11-20 20:08:13 -0600

    Conexión con Axios a la Rick and Morty API

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, Button } from "@mui/material";
 import { useNotification } from "../../context/notification.context";
 import { HeaderComponent } from "components";
+import { characters } from "api/characters";
 
 export const HomePage: React.FC<{}> = () => {
+  useEffect(() => {
+    characters
+      .getById({ id: 1 })
+      .then((r) => {
+        console.log(r.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, []);
   return (
     <Container sx={{ mt: 9 }} maxWidth="xl">
       <HeaderComponent
