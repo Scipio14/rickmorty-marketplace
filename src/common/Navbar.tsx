47561868e9bcbb5cc95b59c82ddd1db874c8dbfa
Federico Macías <fma.abogado@gmail.com>
2022-11-25 17:54:45 -0600

    Agregando el botón de carrito y la logica para eliminar personajes del carrito

--- src/common/Navbar.tsx
+++ src/common/Navbar.tsx
@@ -8,11 +8,22 @@ import {
   Button,
   Typography,
   Stack,
+  IconButton,
+  Badge,
 } from "@mui/material";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 import { useNavigate } from "react-router-dom";
+import { useAppSelector } from "redux/hooks";
+import { CartComponent } from "./Cart";
 
 export const Navbar: React.FC<{}> = () => {
   const navigate = useNavigate();
+  const items = useAppSelector((state) => state.cart);
+  const [open, setOpen] = React.useState<boolean>(false);
+
+  const handleStateViewDrawer = () => {
+    setOpen((state) => !state);
+  };
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="sticky">
@@ -29,6 +40,14 @@ export const Navbar: React.FC<{}> = () => {
               </Grid>
               <Grid item>
                 <Stack direction="row" spacing={2}>
+                  <IconButton
+                    color="primary"
+                    onClick={() => handleStateViewDrawer()}
+                  >
+                    <Badge color="error" badgeContent={items.length}>
+                      <ShoppingCartOutlinedIcon />
+                    </Badge>
+                  </IconButton>
                   <Button variant="contained" onClick={() => navigate("login")}>
                     Login
                   </Button>
@@ -39,6 +58,10 @@ export const Navbar: React.FC<{}> = () => {
           </Container>
         </Toolbar>
       </AppBar>
+      <CartComponent
+        open={open}
+        handleStateViewDrawer={handleStateViewDrawer}
+      />
     </Box>
   );
 };
